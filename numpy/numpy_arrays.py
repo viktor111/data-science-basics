18f181bebe96e2be8c2335fabc64b3e733319599
Viktor <swifrorlilko@gmail.com>
2020-02-15 16:35:11 +0200

    fraction array

--- numpy/numpy_arrays.py
+++ numpy/numpy_arrays.py
@@ -12,3 +12,19 @@ np_matrix = np.array(matrix)
 
 print(np_matrix)
 
+np_range_even = np.arange(0, 10, 2)
+
+print(np_range_even)
+
+np_zeros = np.zeros(3)
+
+print(np_zeros)
+
+np_matrix_shape = np.zeros((4,10))
+
+print(np_matrix_shape)
+
+np_fraction_array = np.linspace(0, 10, 21)
+
+print(np_fraction_array)
+
