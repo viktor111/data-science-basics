f71f0b601644ce21e9b51c054222db62e606e8c7
Viktor <swifrorlilko@gmail.com>
2020-02-15 16:14:05 +0200

    numpy matrix

---
+++ numpy/numpy_arrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+my_list = [1,2,3,4]
+
+numpy_array = np.array(my_list)
+
+print(numpy_array)
+
+matrix = [[1, 2], [3, 4], [5, 6]]
+
+np_matrix = np.array(matrix)
+
+print(np_matrix)
+
